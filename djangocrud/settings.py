e88d55a3df22c3104ff4ff0eb9de9c3bce9544f1
Rosalio Morales <chaliomorales46@gmail.com>
2024-10-20 06:12:24 -0600

    reacomodando static files

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from logging import DEBUG
 from pathlib import Path
 import dj_database_url
 import os.path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,14 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-a8#6(%6(r(x0t4u4xo#whyry(&t#)!u7pge5ysvwkvb%wt+1g*'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
-DEBUG='RENDER' not in os.environ
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
